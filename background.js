876ebe58b7f3077cf766c1752de756b39c1d3507
Eric Halvorsen <viscousness@gmail.com>
2019-11-21 09:10:11 -0500

    Updates to use only /pull/ urls

--- background.js
+++ background.js
@@ -1,13 +1,16 @@
 var enabled = true;
 
+// Rewrite url if enabled to contain ?w=1
 function renavigate(details) {
   if (!enabled) return;
+
   var newUrl = new URL(details.url);
   if (newUrl.searchParams.get('w') === '1') return;
   newUrl.searchParams.set('w', '1');
   return { redirectUrl: newUrl.href };
 }
 
+// Toggles on/off for extension
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.cmd === "setOnOffState") {
@@ -22,6 +25,6 @@ chrome.runtime.onMessage.addListener(
 
 chrome.webRequest.onBeforeRequest.addListener(
   renavigate,
-  { urls: ["*://github.com/*"] },
+  { urls: ["*://github.com/*/pull/*"] },
   ["blocking"]
 );
